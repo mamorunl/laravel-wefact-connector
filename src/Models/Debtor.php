d9a515b4ce23a40996a1a0b351515ac34700f326
Bas Hepping <heppinglbs@gmail.com>
2018-01-25 13:44:00 +0100

    Update Debtor to make it authenticatable

--- src/Models/Debtor.php
+++ src/Models/Debtor.php
@@ -9,8 +9,9 @@
 namespace mamorunl\WeFact\Models;
 
 use mamorunl\WeFact\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-class Debtor extends Model
+class Debtor extends Model implements Authenticatable
 {
     protected $fillable = [
         'Identifier',
@@ -106,5 +107,62 @@ class Debtor extends Model
     public function getId()
     {
         return $this->Identifier;
+    }
+
+    /**
+     * Get the name of the unique identifier for the user.
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return "Identifier";
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->Identifier;
+    }
+
+    /**
+     * Get the password for the user.
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        // TODO: Implement getAuthPassword() method.
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        // TODO: Implement getRememberToken() method.
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string $value
+     *
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        // TODO: Implement setRememberToken() method.
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        // TODO: Implement getRememberTokenName() method.
     }
 }
