085f2540cf4489a76b5977dd4c5e5ac89e6c6631
Bas Hepping <heppinglbs@gmail.com>
2018-01-30 15:02:52 +0100

    Replace the weird 'pluralization' with the laravel str_plural method

--- src/Model.php
+++ src/Model.php
@@ -69,7 +69,7 @@ class Model
         if (!strcmp($data['status'], 'success')) {
             $classes = [];
 
-            foreach ($data[strtolower($base_class) . 's'] as $object) {
+            foreach ($data[str_plural(strtolower($base_class))] as $object) {
                 $classes[] = new $full_class($object);
             }
 
